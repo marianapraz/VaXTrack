861f0816c0849ba57ff7d59e6abc30168d6327f5
Mariana Prazeres <mari_p@live.com.pt>
2021-10-18 16:24:52 +0100

    Improvements on viz

--- dashboard.py
+++ dashboard.py
@@ -22,6 +22,7 @@ def load_data():
     df_vacc = df_vacc[['iso_code', 'location', 'date',
                        'new_vaccinations_smoothed_per_million']
     ].rename({'new_vaccinations_smoothed_per_million': 'vaccines'}).fillna(0)
+    # df_vacc['date'] = pd.to_datetime(df_vacc['date'])
 
     df_miss = pd.read_csv('data/fake_news_list.csv')
     df_miss['location'] = df_miss['location'].str.split(
@@ -36,8 +37,8 @@ st.title('Misinformation versus Vaccine Uptake - V1')
 df_vacc, df_miss = load_data()
 
 # st.subheader('Misinformation hotspots')
-df_miss_countries = df_miss.groupby(['date','location']).count(
-    ).reset_index()
+# df_miss_countries = df_miss.groupby(['date','location']).count(
+#     ).reset_index()
 
 # st.write('Dataset')
 # if st.checkbox('show data for misinformation'):
@@ -53,30 +54,54 @@ st.write('Too see further: https://plotly.com/python/choropleth-maps/')
 
 # st.write(df_miss_countries["location"])
 # st.write(mp["features"][0]["properties"])
-df_vaccdf_vacc_no_date = df_vacc.groupby('iso_code').sum().reset_index()
-fig = px.choropleth(df_vacc,
+# df_vaccdf_vacc_no_date = df_vacc.groupby('iso_code').sum().reset_index()
+df_vacc['week'] = pd.to_datetime(df_vacc['date']).dt.week
+df_vacc['year'] = pd.to_datetime(df_vacc['date']).dt.year
+df_vacc = df_vacc[df_vacc['year'] == 2021]
+df_vacc['week'] = (pd.to_datetime(df_vacc.year.astype(str), format='%Y') + \
+             pd.to_timedelta(df_vacc.week.mul(7).astype(str) + ' days')
+                   ).dt.strftime('%Y-%m-%d')
+
+df_vacc_weekly = df_vacc[['week','iso_code',
+                          'new_vaccinations_smoothed_per_million']].groupby([
+    'week', 'iso_code']).sum().reset_index()
+# st.write(df_vacc_weekly)
+
+fig_vacc = px.choropleth(df_vacc_weekly, #df_vacc,
                     locations="iso_code",
                     # geojson=mp,
                     # featureidkey="properties.ADMIN",
                     color="new_vaccinations_smoothed_per_million",
-                    color_continuous_scale="Viridis",
-                    animation_frame="date",
+                    color_continuous_scale="Blues",
+                    animation_frame="week",
                     scope='world',
                     labels={'new_vaccinations_smoothed_per_million': 'Vaccines'},
                     title='<b>Vaccines around the world</b>',
+range_color=(0, 50000),
                     # hover_name='province',
                     # hover_data={
                     #     'cases' : True,
                     #     'cartodb_id' : False
                     # },
-                    # height=900,
-                    # locationmode='geojson-id',
+                    height=500, width=700,
+                    projection="equirectangular"
                     )
-# fit to area of interest
-# fig.update_geos(fitbounds="locations", visible=True)
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-st.plotly_chart(fig)
+# fit to area of interest
+fig_vacc.update_geos(fitbounds="geojson", visible=False)
+fig_vacc.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                  )
+fig_vacc["layout"].pop("updatemenus")
+fig_vacc.update_layout(coloraxis_colorbar=dict(
+    thicknessmode="pixels", thickness=50,
+    lenmode="pixels", len=200,
+    yanchor="top", y=1,
+    ticks="outside",
+    dtick=5
+))
+
+
+st.plotly_chart(fig_vacc)
 
 st.subheader('Map - Misinformation hotspots')
 st.write('Too see further: https://plotly.com/python/bubble-maps/')
@@ -97,34 +122,52 @@ df_miss['iso'] = df_iso.loc[df_miss['location'].values]['Alpha-3 code'].values
 df_miss_countries = df_miss.groupby(['date','iso']).count(
     ).reset_index()
 
-df_miss_countries_no_date = df_miss_countries.groupby('iso').sum().reset_index()
+# df_miss_countries_no_date = df_miss_countries.groupby('iso').sum().reset_index()
 # st.write(df_miss_countries)
 # st.write(df_miss_countries.shape)
-fig = px.scatter_geo(df_miss_countries,
+df_miss_countries['week'] = pd.to_datetime(df_miss_countries['date']).dt.week
+df_miss_countries['year'] = pd.to_datetime(df_miss_countries['date']).dt.year
+df_miss_countries = df_miss_countries[df_miss_countries['year'] == 2021]
+df_miss_countries['week'] = (pd.to_datetime(df_miss_countries.year.astype(str), format='%Y') + \
+             pd.to_timedelta(df_miss_countries.week.mul(7).astype(str) + ' days')
+                   ).dt.strftime('%Y-%m-%d')
+
+df_miss_weekly = df_miss_countries[['week','iso',
+                          'rating']].groupby([
+    'week', 'iso']).sum().reset_index()
+
+
+# st.write(df_miss_weekly)
+
+fig_miss = px.scatter_geo(df_miss_weekly, #df_miss_countries,
                     locations="iso",
-                    # geojson=mp,
-                    # featureidkey="properties.ADMIN",
                     color="rating",
-                    color_continuous_scale="Viridis",
-                    animation_frame="date",
+                    color_continuous_scale="BuPu",
+                    animation_frame="week",
                     hover_name="iso", size="rating",
                     labels={'rating': 'Misinformation'},
                     title='<b>Misinformation around the world</b>',
-                    # hover_data={
-                    #     'cases' : True,
-                    #     'cartodb_id' : False
-                    # },
-                    # height=900,
-                    # locationmode='geojson-id',
+                    hover_data={
+                        'iso' : True,
+                        'week' : False
+                    },
+                   range_color=(0, 15),
+                     height=500, width=700,
+                     projection="equirectangular",#orthographic"
                     )
 # fit to area of interest
-# fig.update_geos(fitbounds="locations", visible=True)
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig_miss.update_geos(fitbounds=False, visible=True)
+fig_miss.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig_miss["layout"].pop("updatemenus")
+fig_miss.update_layout(coloraxis_showscale=False)
+st.plotly_chart(fig_miss)
+
+import plotly.graph_objects as go
+fig = fig_vacc
+# fig.frames = [go.Frame(data=[fig_miss], traces=[0])]*2
+st.write(fig_miss.data)
+fig.add_trace(fig_miss.data[4], animation_frame='date')
 st.plotly_chart(fig)
-
-
-
-
 #
 st.subheader('Misinformation over time in select countries')
 countries = list(df_vacc['location'].drop_duplicates().values)
